09872379e1950f6d61a870c75a6af8031b71edf2
yu-sit <yurysitnikov@yandex.ru>
2024-07-01 21:45:16 +0300

    come on

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        System.out.println("Привет Yandex Practicum");
     }
 }
